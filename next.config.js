f9f166e2832e04b0eb8a17d29b6118c48cc5f93c
Ben Georges <vex0207@hotmail.com>
2026-02-17 14:15:15 -0500

    Added proximachat.com as main redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.proximachat.com" }],
+        destination: "https://proximachat.com/:path*",
+        permanent: true,
+      },
+    ];
+  },  
   images: {
     domains: ["c.animaapp.com"],
   },
